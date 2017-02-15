60b5ed575cf96d48e31c5ec987449c606a9b9a7f
Ryan <ryanjonesirl@gmail.com>
2017-02-14 16:07:16 -0800

    add two ways of delaying the hiding of an element, using JavaScript and JQuery, and the setTimeout() method.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,15 +1,15 @@
 // Box hide/show, this keyword in JavaScript
 
-document.getElementById("box").onclick = function() {
-  var showing = true;
-  if(showing) {
-    this.style.display = "none";
-    showing = false;
-  } else {
-    this.style.display = "block";
-    showing = true;
-  }
-}
+//document.getElementById("box").onclick = function() {
+//  var showing = true;
+//  if(showing) {
+//    this.style.display = "none";
+//    showing = false;
+//  } else {
+//    this.style.display = "block";
+//    showing = true;
+//  }
+//}
 
 // Box hide/show, this keyword in JQuery
 
@@ -26,3 +26,20 @@ document.getElementById("box").onclick = function() {
 //  });
 //});
 
+// Box hide after 3 secs in JavaScript
+
+//document.getElementById("box").onclick = function () {
+//  setTimeout(function() {
+//    document.getElementById("box").style.display = "none";
+//  }, 3000);
+//}
+
+// Box hide after 3 secs in JQuery
+
+$(document).ready(function() {
+  $("#box").on('click', function() {
+    setTimeout(function() {
+      $("#box").hide();
+    }, 3000);
+  });
+});
