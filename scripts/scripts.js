045b6f5720d5a12f61772ef1d6cef3bd0764ac11
Ryan <ryanjonesirl@gmail.com>
2017-02-14 16:29:52 -0800

    add two ways to use the setTimeout() method to randomly display a hidden element and allow the user to click the element to hide it, once it shows.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,4 @@
-// Box hide/show, this keyword in JavaScript
+/*** Box hide/show, 'this' keyword in JavaScript ***/
 
 //document.getElementById("box").onclick = function() {
 //  var showing = true;
@@ -11,7 +11,7 @@
 //  }
 //}
 
-// Box hide/show, this keyword in JQuery
+/*** Box hide/show, 'this' keyword in JQuery ***/
 
 //$(document).ready(function() {
 //  $("div#box").on('click', function() {
@@ -26,7 +26,7 @@
 //  });
 //});
 
-// Box hide after 3 secs in JavaScript
+/*** Box hide after 3 secs in JavaScript ***/
 
 //document.getElementById("box").onclick = function () {
 //  setTimeout(function() {
@@ -34,12 +34,51 @@
 //  }, 3000);
 //}
 
-// Box hide after 3 secs in JQuery
+/*** Box hide after 3 secs in JQuery ***/
+
+//$(document).ready(function() {
+//  $("#box").on('click', function() {
+//    setTimeout(function() {
+//      $("#box").hide();
+//    }, 3000);
+//  });
+//});
+
+/*** Box will show at a random time, between 0-5000 miliseconds in JavaScript ***/
+
+//var random = Math.random();
+//random = random * 5000;
+//random = Math.floor(random);
+//
+//setTimeout(function() {
+//  
+//  document.getElementById("box").style.display="block";
+//}, random);
+//
+//document.getElementById("box").onclick = function() {
+//  this.style.display="none";
+//}
+
+/*** Box will show at a random time, between 0-5000 miliseconds in JQuery ***/
 
 $(document).ready(function() {
+  
+  var random = Math.random();
+  random = random * 5000;
+  random = Math.floor(random);
+  
+  setTimeout(function() {
+    $("#box").show();
+  }, random);
+  
   $("#box").on('click', function() {
-    setTimeout(function() {
-      $("#box").hide();
-    }, 3000);
+    $(this).hide();
   });
 });
+
+
+
+
+
+
+
